9f436f432a8d70416cf058910dcc29868c5eebf1
eeoviosa <oviosae13@gmail.com>
2026-02-10 17:23:07 -0600

    deployment ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv';
 import cors from 'cors';
 import dns from "dns";
+import path from 'path';
 
 import notesRoutes from './routes/notesRoutes.js'
 import { connectDB } from './config/db.js';
@@ -15,15 +16,26 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //middleware
-app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5178', 'http://localhost:3000']
-}));
+if (process.env.NODE_ENV != 'production') {
+    app.use(cors({
+        origin: ['http://localhost:5173']
+    }));
+}
 app.use(express.json()); //use this middleware to parse JSON bodies
 app.use(rateLimiter); // apply rate limiting middleware
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
